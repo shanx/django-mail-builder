6861c27c404634895bb54cd173eb2948a87e264a
Curtis Maloney <curtis@tinbrain.net>
2016-03-03 16:46:22 +1100

    Scan the template just once for blocks

--- mail_builder/builder.py
+++ mail_builder/builder.py
@@ -1,39 +1,44 @@
 from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template import Context
 from django.template.loader import select_template
 from django.template.loader_tags import BlockNode
 
 
-def render_block(template, block_name, context):
-    for node in template.nodelist.get_nodes_by_type(BlockNode):
-        if node.name == block_name:
-            return node.render(context).strip()
-    return None
+def load_template(template_names):
+    if not isinstance(template_names, (list, tuple)):
+        template_names = (template_names,)
+    template = select_template(template_names)
+    # Unwrap for Django 1.8+
+    template = getattr(template, 'template', template)
+
+    return {
+        node.name: node
+        for node in template.nodelist.get_nodes_by_type(BlockNode)
+    }
 
 
 def build_message(template_names, extra_context=None, force_multipart=False,
                   **defaults):
-    if not isinstance(template_names, (list, tuple)):
-        template_names = (template_names,)
-    tmpl = select_template(template_names)
-    tmpl = getattr(tmpl, 'template', tmpl)  # Unwrap for Django 1.8+
+    blocks = load_template(template_names)
 
     if extra_context is None:
         extra_context = {}
+    extra_context = Context(extra_context)
 
     data = dict(defaults)
     data.setdefault('body', '')
 
     # Scalar values
     for field in ('subject', 'from_email', 'body', 'html'):
-        value = render_block(tmpl, field, extra_context)
-        if value is not None:
-            data[field] = value
+        block = blocks.get(field)
+        if block:
+            data[field] = block.render(extra_context).strip()
 
     # List values
     for field in ('to', 'bcc', 'cc', 'reply_to'):
-        value = render_block(tmpl, field, extra_context)
-        if value is not None:
-            data[field] = value.splitlines()
+        block = blocks.get(field)
+        if block:
+            data[field] = block.render(extra_context).splitlines()
 
     html_content = data.pop('html', None)
     if force_multipart or html_content:
