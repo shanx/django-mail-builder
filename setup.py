946f8e76794473e7bec488e828b4df79bf893638
Curtis Maloney <curtis@tinbrain.net>
2016-03-03 14:14:57 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='Django Mail Builder',
+    description='Simple template-based EmailMessage builder for Django',
+    version='0.1',
+    packages=['mail_builder'],
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    license='BSD',
+    keywords='django email template',
+    url='https://github.com/funkybob/django-mail-builder',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: PyPy3',
+    ],
+)
